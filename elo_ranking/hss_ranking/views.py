5e55124ea63b9736dbe9bd240c1f3ac32f4bce60
Sandy <amwelch@umich.edu>
2015-03-14 13:26:57 -0400

    move scrape into django project and simple datatable working

--- elo_ranking/hss_ranking/views.py
+++ elo_ranking/hss_ranking/views.py
@@ -2,11 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
+import scripts.scrape as scrape
+
 # Create your views here.
+
+
 
 def index(request):
     template = loader.get_template('hss_ranking/index.html')
+    df = scrape.parse_season(max_pages=5)
+
+    data = []
+    rank = 1
+    for index, row in df.iterrows():
+        data.append([rank, row[0], '10', '5', row[1]])
+        rank += 1
     context = RequestContext(request, {
-        'foo': ['bar', 'baz'],
+        'team_rankings': data
     })
+#    context = RequestContext(request, {
+#        'team_rankings': [
+#          [1, 'Huron', '4', '2', '1200'],
+#          [2, 'Pioneer', '2', '4', '1100']],
+#    })
     return HttpResponse(template.render(context))
